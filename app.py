5b79fcaca7200d8eda7e2378e32ef1630ea7b17e
emooney <eric.mooney@gmail.com>
2024-12-10 12:24:02 -0500

    app now runs outside of network

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ def get_lyrics_by_file(filename):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
